33092292cd488928783b851673f3be215c7b07c7
TheSimpleDepartment <111022257+TheSimpleDepartment@users.noreply.github.com>
2024-05-16 23:39:44 -0700

    created new branch

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('pages/dashboard.html', views.dashboard, name='dashboard'),
     path('pages/ecozlounge.html', views.ecozlounge, name='ecozlounge'),
     path('pages/dem.html', views.dem, name='dem'),
+    path('generate-plot-data/', views.generate_plot_data, name='generate_plot_data'),
+    path('pages/partials/plot.html', views.render_plot, name='render_plot'),
+
 ]
